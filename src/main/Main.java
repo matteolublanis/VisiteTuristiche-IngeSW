15ac51af9c52da13742c7e22d02492e602d4dbdd
Matteo Lublanis <matteo.lub02@gmail.com>
2025-04-16 17:59:21 +0200

    Aggiunti commenti

--- src/main/Main.java
+++ src/main/Main.java
@@ -10,7 +10,10 @@ import archivio.ArchivioJSON;
 import controller.ControllerArchivio;
 import controller.Login;
 
-
+/*
+ * Progetto di ingegneria del software: Visite Turistiche
+ * @author David Brembati (741041), Matteo Lublanis(736418), Singh Sahiljit (740552)
+ */
 public class Main {
 	public static void main (String args[]) throws IllegalAccessException, InvocationTargetException, UnsupportedAudioFileException, IOException, LineUnavailableException {
     		ArchivioJSON d = new ArchivioJSON();
